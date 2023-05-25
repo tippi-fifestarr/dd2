ae6913269a23caf1cf8e8f491792543837aa1a00
tippi fifestarr <tippi@boober.org>
2023-05-25 17:35:58 -0500

    main update, prepped for claim page

---
+++ const/myDetails.js
@@ -0,0 +1,7 @@
+// /const/myDetails.js
+// the Access Card (edition drop) contract address as a string
+export const myEditionDropAddress =
+  "0xe003E4487f62cf8fa6a84C517293780b85aedb86";
+
+// the tokenId for the specific asset
+export const myTokenId = "0";
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, ChainId, useContract } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import Head from "next/head";
 import { Gorditas } from "next/font/google";
 
 const gorditas = Gorditas({
@@ -17,15 +18,31 @@ const gorditas = Gorditas({
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
+      <Head>
+        <title>D=D Access Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="DaDeuce, and you can too!" />
+        <meta
+          name="keywords"
+          content="DaDeuce, Thirdweb, Web3, NFT, DAO, DeDeux"
+        />
+        <meta name="author" content="Tippi Fifestarr" />
+      </Head>
       <main className={`${gorditas.variable}`}>
         <Component {...pageProps} />
       </main>
     </ThirdwebProvider>
+  );
+}
+
+function Component() {
+  const { contract, isLoading } = useContract(
+    "0xe003E4487f62cf8fa6a84C517293780b85aedb86"
   );
 }
 
