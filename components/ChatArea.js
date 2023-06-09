b342424bf8de04bf96a2b82884aedfac375a6d18
wyy511511 <wuyaoyao0309@163.com>
2023-06-08 19:40:40 -0600

    print history and buld room

--- components/ChatArea.js
+++ components/ChatArea.js
@@ -1,32 +1,90 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 const ChatArea = () => {
+  const [socket, setSocket] = useState(null);
+  const [messageHistory, setMessageHistory] = useState([]);
+
   useEffect(() => {
     // 在客户端上创建Socket.IO连接
-    // const socket = io();
     const socket = io('http://localhost:3002');
 
     // 监听连接事件
     socket.on('connect', () => {
       console.log('WebSocket connected');
-      socket.emit('message', 'Hello, server!');
+      
+      // Join a room
+      const roomId = "yourRoomId";
+      socket.emit('joinRoom', roomId);
+
       // 发送消息等操作
+      const message = 'Hello, server!';
+      setMessageHistory(prevMessages => [...prevMessages, message]);
+      socket.emit('message', {roomId: roomId, message: message});
     });
 
     // 监听自定义事件或消息
     socket.on('message', (data) => {
       console.log('Client Received message:', data);
       // 处理接收到的消息
+      setMessageHistory(prevMessages => [...prevMessages, data]);
     });
+
+    setSocket(socket);
 
     // 在组件卸载时断开连接
     return () => {
+      console.log('Message history: ', messageHistory);
       socket.disconnect();
     };
   }, []);
 
-  return <div>Socket.IO hhhhhhh example</div>;
+  const disconnect = () => {
+    console.log('Message history: ', messageHistory);
+    socket.disconnect();
+  };
+
+  return (
+    <div>
+      Socket.IO hhhhhhh example
+      <button onClick={disconnect}>Exit</button>
+    </div>
+  );
 };
 
 export default ChatArea;
+
+
+// const ChatArea = () => {
+//   useEffect(() => {
+//     // 在客户端上创建Socket.IO连接
+//     // const socket = io();
+//     const socket = io('http://localhost:3002');
+
+//     // 监听连接事件
+//     socket.on('connect', () => {
+//       console.log('WebSocket connected');
+//       socket.emit('message', 'Hello, server!');
+//       // 发送消息等操作
+//     });
+
+//     // 监听自定义事件或消息
+//     socket.on('message', (data) => {
+//       console.log('Client Received message:', data);
+//       // 处理接收到的消息
+//     });
+
+//     // 在组件卸载时断开连接
+//     return () => {
+//       socket.disconnect();
+//     };
+//   }, []);
+
+//   return <div>Socket.IO hhhhhhh example</div>;
+// };
+
+// export default ChatArea;
+
+
+// https://tailwind-elements.com/docs/standard/forms/inputs/
+
--- server.js
+++ server.js
@@ -20,21 +20,49 @@ app.use(cors());
 
 
 
+// ...
+
+let rooms = {}
+let messageHistory = {}
+
 io.on('connection', (socket) => {
   console.log('A client connected');
 
+  socket.on('joinRoom', (roomId) => {
+    if (rooms[roomId]) {
+      rooms[roomId].push(socket.id)
+    } else {
+      rooms[roomId] = [socket.id]
+    }
+    messageHistory[socket.id] = []
+    socket.join(roomId);
+  });
+
   socket.on('message', (data) => {
     console.log('Server Received message:', data);
-    // 处理接收到的消息
+    
+    messageHistory[socket.id].push(data.message);
 
-    // 发送消息给客户端
-    socket.emit('message', 'Server says hello!');
+    // 发送消息给同一个房间的所有客户端
+    io.to(data.roomId).emit('message', data.message);
   });
 
   socket.on('disconnect', () => {
+    // Remove the disconnected socket from all rooms
+    for(let room in rooms) {
+      const index = rooms[room].indexOf(socket.id);
+      if (index > -1) {
+        rooms[room].splice(index, 1);
+      }
+    }
+    console.log(`Message history for client ${socket.id}: `, messageHistory[socket.id])
+    delete messageHistory[socket.id];
     console.log('A client disconnected');
   });
 });
+
+// ...
+
 
 server.listen(3002, () => {
   console.log('Socket.IO server listening on port 3002');
