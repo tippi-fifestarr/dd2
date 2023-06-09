5796b0807c3646d476bb625f269e1bb67d7eb60c
tippi fifestarr <tippi@boober.org>
2023-06-09 12:01:47 -0500

    commented out the ranked nft for now

--- components/RankedNFT.jsx
+++ components/RankedNFT.jsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from "react";
-import {
-  ThirdwebNftMedia,
-  useNFT,
-  useContract,
-  useContractWrite,
-} from "@thirdweb-dev/react";
+// import { useEffect, useState } from "react";
+// import {
+//   ThirdwebNftMedia,
+//   useNFT,
+//   useContract,
+//   useContractWrite,
+// } from "@thirdweb-dev/react";
 
-const RankedNFT = (userWalletAddress) => {
-  const { contract, isLoading } = useContract(
-    "0x8c98434b7CA12571568065ab2fbE2248fA0bA504"
-  );
-  const {
-    data: ownerBalance,
-    isLoading,
-    error,
-  } = useNFTBalance(contract, userWalletAddress);
-  // for ERC1155 contracts, you can also pass a tokenId
+// const RankedNFT = (userWalletAddress) => {
+//   const { contract, isLoading } = useContract(
+//     "0x8c98434b7CA12571568065ab2fbE2248fA0bA504"
+//   );
+//   const {
+//     data: ownerBalance,
+//     isLoading,
+//     error,
+//   } = useNFTBalance(contract, userWalletAddress);
+//   // for ERC1155 contracts, you can also pass a tokenId
 
-  return (
-    <div>
-      {!isLoading && !error && (
-        <div>
-          <h1 className="font-gorditas text-lg text-center m-1">
-            Buy for $0.02 in MATIC to enable Ranked Matches!
-          </h1>
-          {nft && (
-            <ThirdwebNftMedia metadata={nft?.metadata} className="rounded-xl" />
-          )}
-        </div>
-      )}
-      {isLoading ? <div>Loading...</div> : <div>Error: {error.message}</div>}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {!isLoading && !error && (
+//         <div>
+//           <h1 className="font-gorditas text-lg text-center m-1">
+//             Buy for $0.02 in MATIC to enable Ranked Matches!
+//           </h1>
+//           {nft && (
+//             <ThirdwebNftMedia metadata={nft?.metadata} className="rounded-xl" />
+//           )}
+//         </div>
+//       )}
+//       {isLoading ? <div>Loading...</div> : <div>Error: {error.message}</div>}
+//     </div>
+//   );
+// };
 
-export default RankedNFT;
+// export default RankedNFT;
